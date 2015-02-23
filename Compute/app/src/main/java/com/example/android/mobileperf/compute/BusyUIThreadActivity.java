4b215f2b882bfa1510903f8013b632403c59e0d5
Joanna Smith <joannasmith@google.com>
2015-02-23 15:30:16 -0800

    Moving image manipulation off the main thread to prevent the UI from hanging

--- Compute/app/src/main/java/com/example/android/mobileperf/compute/BusyUIThreadActivity.java
+++ Compute/app/src/main/java/com/example/android/mobileperf/compute/BusyUIThreadActivity.java
@@ -19,6 +19,7 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebView;
@@ -59,39 +60,53 @@ public class BusyUIThreadActivity extends Activity {
         // Challenge accepted!
         Bitmap loadedBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.punk_droid);
 
-        int width = loadedBitmap.getWidth();
-        int height = loadedBitmap.getHeight();
-        Bitmap sepiaBitmap = Bitmap.createBitmap(width, height, loadedBitmap.getConfig());
+        // By performing the sepia filter manipulations in an AsyncTask, our UI can continue while
+        // the CPU is processing the filter. Notice how the dancing pirate never misses a beat now?
+        new SepiaFilterTask().execute(loadedBitmap);
+    }
 
-        // Go through every single pixel in the bitmap, apply a sepia filter to it, and apply it
-        // to the new "output" bitmap.
-        for(int x = 0; x < width; ++x) {
-            for(int y = 0; y < height; ++y) {
-                // get pixel color
-                int pixel = loadedBitmap.getPixel(x, y);
-                int alpha = Color.alpha(pixel);
+    private class SepiaFilterTask extends AsyncTask<Bitmap, Void, Bitmap> {
+        protected Bitmap doInBackground(Bitmap... bitmaps) {
+            Bitmap loadedBitmap = bitmaps[0];
 
-                // These are the starting values for this pixel.
-                int inputRed = Color.red(pixel);
-                int inputGreen = Color.green(pixel);
-                int inputBlue = Color.blue(pixel);
+            int width = loadedBitmap.getWidth();
+            int height = loadedBitmap.getHeight();
+            Bitmap sepiaBitmap = Bitmap.createBitmap(width, height, loadedBitmap.getConfig());
 
-                // These are the sepia-fied values for each pixel.
-                // Note that if the resulting value is over 255, Math.Min brings it back down.
-                // So effectively, min establishes a max value!  Isn't that weird?
-                int outRed = (int) Math.min(
-                        (inputRed * .393) + (inputGreen *.769) + (inputBlue * .189), 255);
+            // Go through every single pixel in the bitmap, apply a sepia filter to it, and apply it
+            // to the new "output" bitmap.
+            for(int x = 0; x < width; ++x) {
+                for(int y = 0; y < height; ++y) {
+                    // get pixel color
+                    int pixel = loadedBitmap.getPixel(x, y);
+                    int alpha = Color.alpha(pixel);
 
-                int outGreen = (int) Math.min(
-                        (inputRed * .349) + (inputGreen *.686) + (inputBlue * .168), 255);
+                    // These are the starting values for this pixel.
+                    int inputRed = Color.red(pixel);
+                    int inputGreen = Color.green(pixel);
+                    int inputBlue = Color.blue(pixel);
 
-                int outBlue = (int) Math.min(
-                        (inputRed * .272) + (inputGreen *.534) + (inputBlue * .131), 255);
-                // apply new pixel color to output bitmap
-                sepiaBitmap.setPixel(x, y, Color.argb(alpha, outRed, outGreen, outBlue));
+                    // These are the sepia-fied values for each pixel.
+                    // Note that if the resulting value is over 255, Math.Min brings it back down.
+                    // So effectively, min establishes a max value!  Isn't that weird?
+                    int outRed = (int) Math.min(
+                            (inputRed * .393) + (inputGreen *.769) + (inputBlue * .189), 255);
+
+                    int outGreen = (int) Math.min(
+                            (inputRed * .349) + (inputGreen *.686) + (inputBlue * .168), 255);
+
+                    int outBlue = (int) Math.min(
+                            (inputRed * .272) + (inputGreen *.534) + (inputBlue * .131), 255);
+                    // apply new pixel color to output bitmap
+                    sepiaBitmap.setPixel(x, y, Color.argb(alpha, outRed, outGreen, outBlue));
+                }
             }
+            return sepiaBitmap;
         }
-        ImageView imageView = (ImageView) findViewById(R.id.busyui_imageview);
-        imageView.setImageBitmap(sepiaBitmap);
+
+        protected void onPostExecute(Bitmap sepiaBitmap) {
+            ImageView imageView = (ImageView) findViewById(R.id.busyui_imageview);
+            imageView.setImageBitmap(sepiaBitmap);
+        }
     }
 }
